c59dae38aa5976708abe3a3845e28be6d5a387fc
Seif Tabaja <saif_tabaja@hotmail.com>
2019-02-13 22:21:55 +0100

    fixed part-1 and part-1 based on given comments

--- Week2/homework/part-1/queries.js
+++ Week2/homework/part-1/queries.js
@@ -21,58 +21,62 @@ connection.connect(err => {
 //What is the capital of country X ? (Accept X from user)
 app.get("/query/country/:countryName", (req, res) => {
   console.log(req.params.countryName);
-  let query = `select name from city where id = (select capital from country where name = "${
-    req.params.countryName
-  }")`;
-  connection.query(query, (err, result) => {
-    if (err) throw err;
-    //console.log(result);
-    res.send(JSON.stringify({ result }));
-  });
+  let query = `select name from city where id = (select capital from country where name = ?)`;
+  connection.query(
+    query,
+    [req.params.countryName],
+    (err, result) => {
+      if (err) throw err;
+      //console.log(result);
+      res.send(JSON.stringify({ result }));
+    }
+  );
 });
 
 //List all the languages spoken in the region Y (Accept Y from user)
 app.get("/query/region/:region", (req, res) => {
   console.log(req.params.region);
-  let query = `select DISTINCT language from CountryLanguage where CountryCode in (select Code from country where Region = "${
-    req.params.region
-  }")`;
-  connection.query(query, (err, result) => {
-    if (err) throw err;
-    //console.log(result);
-    res.send(JSON.stringify({ result }));
-  });
+  let query = `select DISTINCT language from CountryLanguage where CountryCode in (select Code from country where Region = ?)`;
+  connection.query(
+    query,
+    [req.params.region],
+    (err, result) => {
+      if (err) throw err;
+      //console.log(result);
+      res.send(JSON.stringify({ result }));
+    }
+  );
 });
 
 //Find the number of cities in which language Z is spoken (Accept Z from user)
 app.get("/query/city/:language", (req, res) => {
   console.log(req.params.language);
-  let query = `select count(*) from city where CountryCode in (select CountryCode from countryLanguage where language = "${
-    req.params.language
-  }")`;
-  connection.query(query, (err, result) => {
-    if (err) throw err;
-    //console.log(result);
-    res.send(JSON.stringify({ result }));
-  });
-});
-//question 4
-app.get(
-  "/query/countryRegion/:country/:lang/:isOff",
-  (req, res) => {
-    console.log(req.params.country);
-    let query = `select name from country where region in (select region from country where name = "${
-      req.params.country
-    }" AND code in (select CountryCode from CountryLanguage where Language = "${
-      req.params.lang
-    }" AND isOfficial = "${req.params.isOff}"))`;
-    connection.query(query, (err, result) => {
+  let query = `select count(*) from city where CountryCode in (select CountryCode from countryLanguage where language = ?)`;
+  connection.query(
+    query,
+    [req.params.language],
+    (err, result) => {
       if (err) throw err;
       //console.log(result);
       res.send(JSON.stringify({ result }));
-    });
-  }
-);
+    }
+  );
+});
+//question 4
+app.get("/query/countryRegion/:country", (req, res) => {
+  console.log(req.params.country);
+  let query = `select name from country where region in (select region from country where name = ?
+       AND code in (select CountryCode from CountryLanguage where Language = "Arabic" AND isOfficial = "T"))`;
+  connection.query(
+    query,
+    [req.params.country],
+    (err, result) => {
+      if (err) throw err;
+      //console.log(result);
+      res.send(JSON.stringify({ result }));
+    }
+  );
+});
 
 //List all the continents with the number of languages spoken in each continent
 app.get("/query/continent", (req, res) => {
--- Week2/homework/part-2/insertAlert.js
+++ Week2/homework/part-2/insertAlert.js
@@ -19,32 +19,41 @@ connection.connect(err => {
   console.log("connected");
 });
 
-app.get("/query/countrylanguage/:code", (req, res) => {
-  console.log(req.params.code);
-  let query = `INSERT INTO countrylanguage (countrycode, language, isOfficial) VALUES ("${
-    req.params.code
-  }", 'Arabic', 'T');`;
+app.get(
+  "/query/countrylanguage/:code/:lang/:isOff",
+  (req, res) => {
+    console.log(
+      req.params.code +
+        " " +
+        req.params.lang +
+        " " +
+        req.params.isOff
+    );
+    let query = `INSERT INTO countrylanguage (countrycode, language, isOfficial) VALUES ("${
+      req.params.code
+    }", "${req.params.lang}", "${req.params.isOff}");`;
 
-  let q2 = `select count(language) AS languageNumber from countryLanguage where countryCode = "${
-    req.params.code
-  }"`;
-  connection.query(query, (err, result) => {
-    if (err) throw err;
-    //console.log(result);
-    res.send(JSON.stringify({ result }));
-    connection.query(q2, (err, result) => {
+    let q2 = `select count(language) AS languageNumber from countryLanguage where countryCode = "${
+      req.params.code
+    }"`;
+    connection.query(query, (err, result) => {
       if (err) throw err;
       //console.log(result);
-      if (result[0].languageNumber >= 10) {
-        JSAlert.alert(
-          "How can I achieve this .. 10 or more LANGS ?"
-        );
-      } else {
-        JSAlert.alert("Less than 10 LANGS");
-      }
+      res.send(JSON.stringify({ result }));
+      connection.query(q2, (err, result) => {
+        if (err) throw err;
+        //console.log(result);
+        if (result[0].languageNumber >= 10) {
+          JSAlert.alert(
+            "How can I achieve this .. 10 or more LANGS ?"
+          );
+        } else {
+          JSAlert.alert("Less than 10 LANGS");
+        }
+      });
     });
-  });
-});
+  }
+);
 
 app.listen("3000", () => {
   console.log("Server startred on port 3000");
